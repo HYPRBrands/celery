f354dc8fe3f59ead62b4705da82b79e894bfb644
Lior May Nissim <lior@hyprbrands.com>
2018-10-09 17:03:59 +0300

    this is a patch version

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'latentcall'
 
-__version__ = '4.1.0'
+__version__ = '4.1.0-patch'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
